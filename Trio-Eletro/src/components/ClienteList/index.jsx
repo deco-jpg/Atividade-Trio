ed973b1710167826b1ec937350be013a7e6e53e9
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-07-06 20:01:30 -0300

    listar cliente

--- Trio-Eletro/src/components/ClienteList/index.jsx
+++ Trio-Eletro/src/components/ClienteList/index.jsx
@@ -1,1 +1,57 @@
+import { use } from 'react'
 import './style.css'
+
+export default function ClienteList() {
+    const [clientes, setClientes] = useState([])
+
+    useEffect(() => {
+        async function buscarClientes() {
+            try {
+                const reposta = await api.get('/clientes')
+                setClientes(reposta.data)
+            } catch (error) {
+                toast.error('Erro ao buscar clientes')
+            }
+        }
+        buscarClientes()
+
+
+
+    }, [])
+
+    return (
+        <div className='lista-page'>
+            <h1>Listar Clientes</h1>
+            {clientes.length === 0 ? (
+                <p className='lista-vazia'>Não há clientes cadastrados.</p>
+            ) : (
+                <table className='lista-tabela'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th>Telefone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {clientes.map(cliente => (
+                            <tr key={cliente.id}>
+                                <td>{cliente.id}</td>
+                                <td>{cliente.nome}</td>
+                                <td>{cliente.email}</td>
+                                <td>{cliente.telefone}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+
+
+    )
+
+}
+
+
+
