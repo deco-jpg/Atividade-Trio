31e050091332a540f89fff03c86dca5ff2b17f52
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-07-06 20:31:42 -0300

    listarfuncioanrio

--- Trio-Eletro/src/components/Footer/index.jsx
+++ Trio-Eletro/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import './style.css'
 export default function Footer() {
   return (
     <footer>
-      <p>© 2024 Trio Eletro. Todos os direitos reservados.</p>
+      <p>© 2026 Trio Eletro. Todos os direitos reservados.</p>
     </footer>
   )
 }
