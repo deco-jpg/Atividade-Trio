74782084f27431fadd24cce209687141809a0841
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-07-06 20:34:56 -0300

    consertando erros

--- Trio-Eletro/src/App.jsx
+++ Trio-Eletro/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import CadastroClientePage from './pages/CadastroClientePage'
