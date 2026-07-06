8b20903ca5e62fbb08c152ad3e3c51a8c929e8b1
AlvaroCoelho <alvaro.coelho@ba.estudante.senai.br>
2026-07-06 19:31:39 -0300

    Add files via upload

---
+++ trio-eletro-back/src/test/java/com/example/trio_eletro_back/TrioEletroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trio_eletro_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrioEletroBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
